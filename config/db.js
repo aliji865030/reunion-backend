c67335f57ac899d4178234f63e92187ebd214bed
aliji865030 <abbasalichand786@gmail.com>
2024-09-17 07:43:27 +0530

    mongo

--- config/db.js
+++ config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.CONNECTION_STRING, {
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.CONNECTION_STRING);
         console.log('MongoDB connected');
     } catch (error) {
         console.error(error.message);
